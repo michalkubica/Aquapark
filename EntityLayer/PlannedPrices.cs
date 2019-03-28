58c7f307701f405520162c409f816269dcf94751
Michał Kubica <michal.kubica@hotmail.com>
2019-03-28 19:59:52 +0100

    add dir entitylayer

---
+++ EntityLayer/PlannedPrices.cs
@@ -0,0 +1,16 @@
+﻿namespace EntityLayer
+{
+    public class PlannedPrices
+    {
+        public int price { get; set; }
+        public string beginDate { get; set; }
+        public string endDate { get; set; }
+
+        public PlannedPrices(int p, string b, string e)
+        {
+            price = p;
+            beginDate = b;
+            endDate = e;
+        }
+    }
+}
